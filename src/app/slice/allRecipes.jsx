3888aa4a09feed299f667a9a66e3389889db4d12
Pavitrank <pavitrank2002@gmail.com>
2025-08-19 12:22:59 +0530

    "slice to fetch all recipe based on input"

--- src/app/slice/allRecipes.jsx
+++ src/app/slice/allRecipes.jsx
@@ -3,12 +3,15 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 // createAsyncThunk - When we want to do a delayed task we use this createAsyncThunk middleware
 
 // We are creating a action here
-export const fetchAllRecipe = createAsyncThunk("fetchAllRecipe", async () => {
-  const response = await fetch(
-    "https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza&key=d348a0b0-c7b8-4539-b6a6-80f883fdef51"
-  );
-  return response.json();
-});
+export const fetchAllRecipe = createAsyncThunk(
+  "fetchAllRecipe",
+  async (inputV) => {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes?search=${inputV}&key=d348a0b0-c7b8-4539-b6a6-80f883fdef51`
+    );
+    return response.json();
+  }
+);
 const allRecipeSlice = createSlice({
   name: "allRecipe",
   initialState: {
