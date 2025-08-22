60098c091aa23ad4490bda49c1469694fa5222b4
Pavitrank <pavitrank2002@gmail.com>
2025-08-22 12:55:06 +0530

    "UI: Updated Bookmarks container styling"

--- src/Components/Bookmark.jsx
+++ src/Components/Bookmark.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Bookmark.css";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -34,16 +35,19 @@ const Bookmark = ({ isVisible, setIsVisible }) => {
                   <section className="imageContainer">
                     <img src={item.image_url} alt={item.title} />
                   </section>
-                  <section>
+                  <section className="descriptionContainer">
                     <p>{item.title}</p>
                     <p>{item.publisher}</p>
+                  </section>
+                  <section className="userIcon">
+                    <FontAwesomeIcon icon={faUser} />
                   </section>
                 </section>
               </>
             ))
           ) : (
             <section className="emptyContainer">
-            <p>No bookmarks yet</p>
+              <p>No bookmarks yet</p>
             </section>
           )}
         </section>
