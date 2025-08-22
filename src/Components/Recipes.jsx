0fa05885368af3c903aab4149a6e1c93811234d1
Pavitrank <pavitrank2002@gmail.com>
2025-08-22 19:07:36 +0530

    "Added conditonal rendering to update the view"

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -30,6 +30,7 @@ import { incrementServings } from "../app/slice/singleRecipe";
 import { decrementServings } from "../app/slice/singleRecipe";
 import { handleBookmarks } from "../app/slice/bookmarks";
 import Button from "./Button";
+import NewRecipe from "./NewRecipe";
 
 const Recipes = () => {
   const [inputValue, setInputValue] = useState("");
@@ -41,6 +42,7 @@ const Recipes = () => {
   const dispatch = useDispatch();
   const [isBookmarkViewVisible, setIsBookmarkViewVisible] = useState(false);
   const [isAddRecipeVisible, setIsAddRecipeVisible] = useState(false);
+  console.log("The value of recipe visible", isAddRecipeVisible);
 
   const recipes = useSelector(
     (state) => state?.allRecipe?.data?.data?.recipes || []
@@ -145,208 +147,213 @@ const Recipes = () => {
   return (
     <>
       <Header />
-      <section className="recipesContainer">
-        <section className="recipeHeadingContainer">
-          <section className="recipeHeadingLeft">
-            <img src={Logo} alt="Website Logo" />
-            <input
-              type="text"
-              placeholder="Search over 1,00,000 recipes"
-              className="inputField"
-              value={inputValue}
-              onChange={updateInputValue}
-              id="inputFieldId"
-            />
-          </section>
-          <section className="recipeHeadingRight">
-            <section className="bookmarkContainer">
-              <button
-                onClick={handleAddRecipeVisibility}
-                onMouseEnter={handleAddRecipeHoverVisibility}
-                // onMouseLeave={handleAddRecipeVisibility}
-              >
-                ADD RECIPES
-              </button>
-              <button
-                onClick={handleBookmarkViewVisibility}
-                onMouseEnter={handleBookmarkHoverVisiblity}
-                // onMouseLeave={handleBookmarkViewVisibility}
-              >
-                BOOKMARK
-              </button>
-              <FontAwesomeIcon
-                icon={faPlus}
-                size="xl"
-                className="plusIcon"
-                onClick={handleAddRecipeVisibility}
-                onMouseEnter={handleAddRecipeHoverVisibility}
-                // onMouseLeave={handleAddRecipeVisibility}
+      {isAddRecipeVisible ? (
+        <section className="recipesContainer">
+          <section className="recipeHeadingContainer">
+            <section className="recipeHeadingLeft">
+              <img src={Logo} alt="Website Logo" />
+              <input
+                type="text"
+                placeholder="Search over 1,00,000 recipes"
+                className="inputField"
+                value={inputValue}
+                onChange={updateInputValue}
+                id="inputFieldId"
               />
-              <FontAwesomeIcon
-                icon={faBookmark}
-                size="xl"
-                className="bookmarkIcon"
-                onClick={handleBookmarkViewVisibility}
-                onMouseEnter={handleBookmarkHoverVisiblity}
-                // onMouseLeave={handleBookmarkViewVisibility}
-              />
-              {isBookmarkViewVisible ? (
-                <Bookmark
-                  isVisible={isBookmarkViewVisible}
-                  setIsVisible={setIsBookmarkViewVisible}
+            </section>
+            <section className="recipeHeadingRight">
+              <section className="bookmarkContainer">
+                <button
+                  onClick={handleAddRecipeVisibility}
+                >
+                  ADD RECIPES
+                </button>
+                <button
+                  onClick={handleBookmarkViewVisibility}
+                  onMouseEnter={handleBookmarkHoverVisiblity}
+                >
+                  BOOKMARK
+                </button>
+                <FontAwesomeIcon
+                  icon={faPlus}
+                  size="xl"
+                  className="plusIcon"
+                  onClick={handleAddRecipeVisibility}
                 />
+                <FontAwesomeIcon
+                  icon={faBookmark}
+                  size="xl"
+                  className="bookmarkIcon"
+                  onClick={handleBookmarkViewVisibility}
+                  onMouseEnter={handleBookmarkHoverVisiblity}
+                />
+                {isBookmarkViewVisible ? (
+                  <Bookmark
+                    isVisible={isBookmarkViewVisible}
+                    setIsVisible={setIsBookmarkViewVisible}
+                  />
+                ) : (
+                  ""
+                )}
+              </section>
+            </section>
+          </section>
+          <section className="middleContainer">
+            <section className="middleLeft">
+              {currentData &&
+                currentData.length > 0 &&
+                currentData.map((item) => (
+                  <section
+                    className="recipesList"
+                    key={item.id}
+                    onClick={() => handleSingleRecipe(item.id)}
+                  >
+                    <section className="recipesLeft">
+                      <section className="imageContainer">
+                        <img src={item.image_url} alt={item.recipe_title} />
+                      </section>
+                      <section className="nameContainer">
+                        <p className="itemTitle">{item.title}</p>
+                        <p className="itemPublisher">{item.publisher}</p>
+                      </section>
+                    </section>
+                    {currentData && currentData.length > 0 && (
+                      <section className="userContainer">
+                        <FontAwesomeIcon icon={faUser} />
+                      </section>
+                    )}
+                  </section>
+                ))}
+              {currentData?.length > 0 ? (
+                <section className="recipeButtonContainer">
+                  <button
+                    onClick={handlePageDecrement}
+                    className={currentPage == 1 ? "disabled" : ""}
+                  >
+                    PREV
+                  </button>
+                  <button
+                    onClick={handlePageIncrement}
+                    className={currentPage == totalPages ? "disabled" : ""}
+                  >
+                    NEXT
+                  </button>
+                </section>
               ) : (
                 ""
               )}
             </section>
+            <section className="middleRight" key={singleRecipes?.id}>
+              {singleRecipes &&
+              typeof (singleRecipes === "object") &&
+              singleRecipes.length !== 0 ? (
+                <>
+                  <section className="imageContainer">
+                    <img
+                      src={singleRecipes.imageURL || singleRecipes.image_url}
+                    />
+                  </section>
+                  <section className="navigationContainer">
+                    <section className="timingContainer">
+                      <FontAwesomeIcon icon={faClock} className="clockIcon" />
+                      <span className="minutes">
+                        {singleRecipes.cookingTime ||
+                          singleRecipes.cooking_time}{" "}
+                        MINUTES
+                      </span>
+                    </section>
+                    <section className="servingsContainer">
+                      <FontAwesomeIcon icon={faUser} className="servingsIcon" />
+                      <span>{singleRecipes.servings} Servings</span>
+                      <section className="updationContainer">
+                        <FontAwesomeIcon
+                          icon={faMinus}
+                          className="minusIcon"
+                          onClick={handleDecrementServings}
+                        />
+                        <FontAwesomeIcon
+                          icon={faPlus}
+                          className="plusIcon"
+                          onClick={handleIncrementServings}
+                        />
+                      </section>
+                    </section>
+                    <section className="recipeBookmark">
+                      {singleRecipes.isBookmarked ? (
+                        <FontAwesomeIcon
+                          icon={faHeartSolid}
+                          className="recipeBookmarkIcon"
+                          style={{ color: "#f48982" }}
+                          onClick={() => handleBookmark()}
+                        />
+                      ) : (
+                        <FontAwesomeIcon
+                          icon={faHeart}
+                          className="recipeBookmarkIcon"
+                          style={{ color: "#f48982" }}
+                          onClick={() => handleBookmark()}
+                        />
+                      )}
+                    </section>
+                  </section>
+                  <section className="itemListContainer">
+                    <h1>RECIPE INGREDIENTS</h1>
+                    <section className="allItemContainer">
+                      {singleRecipes?.ingredients?.map((ing) => (
+                        <>
+                          <section className="itemContainer">
+                            <span>
+                              <FontAwesomeIcon
+                                icon={faCircleCheck}
+                                className="recipeIcons"
+                              />
+                            </span>
+                            <span>
+                              {ing?.quantity || ""} {ing?.unit || ""}{" "}
+                              {ing?.description || ""}{" "}
+                            </span>
+                          </section>
+                        </>
+                      ))}
+                    </section>
+                  </section>
+                  <section className="directionsContainer">
+                    <h1 id="directionHeading">HOW TO COOK IT</h1>
+                    <p>The recipe was carefully designed and tested by</p>
+                    <p id="publisherName">
+                      {singleRecipes?.recipePublisher ||
+                        singleRecipes?.publisher}
+                    </p>
+                    <p>Please check out directions at their website.</p>
+                    <a
+                      href={singleRecipes.sourceURL || singleRecipes.source_url}
+                    >
+                      <button>DIRECTIONS</button>
+                    </a>
+                  </section>
+                </>
+              ) : (
+                <>
+                  <section className="box1">
+                    <section className="left">
+                      <FontAwesomeIcon
+                        icon={faFaceSmile}
+                        className="smileIcon"
+                      />
+                    </section>
+                    <section className="right">
+                      <p>Start by searching for a recipe</p>
+                      <p>or an ingredient.</p>
+                    </section>
+                  </section>
+                </>
+              )}
+            </section>
           </section>
         </section>
-        <section className="middleContainer">
-          <section className="middleLeft">
-            {currentData &&
-              currentData.length > 0 &&
-              currentData.map((item) => (
-                <section
-                  className="recipesList"
-                  key={item.id}
-                  onClick={() => handleSingleRecipe(item.id)}
-                >
-                  <section className="recipesLeft">
-                    <section className="imageContainer">
-                      <img src={item.image_url} alt={item.recipe_title} />
-                    </section>
-                    <section className="nameContainer">
-                      <p className="itemTitle">{item.title}</p>
-                      <p className="itemPublisher">{item.publisher}</p>
-                    </section>
-                  </section>
-                  {currentData && currentData.length > 0 && (
-                    <section className="userContainer">
-                      <FontAwesomeIcon icon={faUser} />
-                    </section>
-                  )}
-                </section>
-              ))}
-            {currentData?.length > 0 ? (
-              <section className="recipeButtonContainer">
-                <button
-                  onClick={handlePageDecrement}
-                  className={currentPage == 1 ? "disabled" : ""}
-                >
-                  PREV
-                </button>
-                <button
-                  onClick={handlePageIncrement}
-                  className={currentPage == totalPages ? "disabled" : ""}
-                >
-                  NEXT
-                </button>
-              </section>
-            ) : (
-              ""
-            )}
-          </section>
-          <section className="middleRight" key={singleRecipes?.id}>
-            {singleRecipes &&
-            typeof (singleRecipes === "object") &&
-            singleRecipes.length !== 0 ? (
-              <>
-                <section className="imageContainer">
-                  <img
-                    src={singleRecipes.imageURL || singleRecipes.image_url}
-                  />
-                </section>
-                <section className="navigationContainer">
-                  <section className="timingContainer">
-                    <FontAwesomeIcon icon={faClock} className="clockIcon" />
-                    <span className="minutes">
-                      {singleRecipes.cookingTime || singleRecipes.cooking_time}{" "}
-                      MINUTES
-                    </span>
-                  </section>
-                  <section className="servingsContainer">
-                    <FontAwesomeIcon icon={faUser} className="servingsIcon" />
-                    <span>{singleRecipes.servings} Servings</span>
-                    <section className="updationContainer">
-                      <FontAwesomeIcon
-                        icon={faMinus}
-                        className="minusIcon"
-                        onClick={handleDecrementServings}
-                      />
-                      <FontAwesomeIcon
-                        icon={faPlus}
-                        className="plusIcon"
-                        onClick={handleIncrementServings}
-                      />
-                    </section>
-                  </section>
-                  <section className="recipeBookmark">
-                    {singleRecipes.isBookmarked ? (
-                      <FontAwesomeIcon
-                        icon={faHeartSolid}
-                        className="recipeBookmarkIcon"
-                        style={{ color: "#f48982" }}
-                        onClick={() => handleBookmark()}
-                      />
-                    ) : (
-                      <FontAwesomeIcon
-                        icon={faHeart}
-                        className="recipeBookmarkIcon"
-                        style={{ color: "#f48982" }}
-                        onClick={() => handleBookmark()}
-                      />
-                    )}
-                  </section>
-                </section>
-                <section className="itemListContainer">
-                  <h1>RECIPE INGREDIENTS</h1>
-                  <section className="allItemContainer">
-                    {singleRecipes?.ingredients?.map((ing) => (
-                      <>
-                        <section className="itemContainer">
-                          <span>
-                            <FontAwesomeIcon
-                              icon={faCircleCheck}
-                              className="recipeIcons"
-                            />
-                          </span>
-                          <span>
-                            {ing?.quantity || ""} {ing?.unit || ""}{" "}
-                            {ing?.description || ""}{" "}
-                          </span>
-                        </section>
-                      </>
-                    ))}
-                  </section>
-                </section>
-                <section className="directionsContainer">
-                  <h1 id="directionHeading">HOW TO COOK IT</h1>
-                  <p>The recipe was carefully designed and tested by</p>
-                  <p id="publisherName">
-                    {singleRecipes?.recipePublisher || singleRecipes?.publisher}
-                  </p>
-                  <p>Please check out directions at their website.</p>
-                  <a href={singleRecipes.sourceURL || singleRecipes.source_url}>
-                    <button>DIRECTIONS</button>
-                  </a>
-                </section>
-              </>
-            ) : (
-              <>
-                <section className="box1">
-                  <section className="left">
-                    <FontAwesomeIcon icon={faFaceSmile} className="smileIcon" />
-                  </section>
-                  <section className="right">
-                    <p>Start by searching for a recipe</p>
-                    <p>or an ingredient.</p>
-                  </section>
-                </section>
-              </>
-            )}
-          </section>
-        </section>
-      </section>
+      ) : (
+        <NewRecipe isAddRecipeVisible = {isAddRecipeVisible} setIsAddRecipeVisible = {setIsAddRecipeVisible}/>
+      )}
     </>
   );
 };
