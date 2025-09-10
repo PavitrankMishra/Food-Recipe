5aacd9e3c82d50db4601edbe75f4ece356773b70
Pavitrank <pavitrank2002@gmail.com>
2025-09-10 23:26:10 +0530

    "feat: Component to submit Recipe Delete Request"

---
+++ src/Components/Recipes/RecipeDeletePrompt.jsx
@@ -0,0 +1,43 @@
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./RecipeDeletePrompt.css";
+
+const RecipeDeletePrompt = ({
+  singleRecipes,
+  isTrashClicked,
+  setTrashClicked,
+  handleRecipeDelete,
+}) => {
+  const handleClosePrompt = () => {
+    setTrashClicked(false);
+  };
+  return (
+    <>
+      <section className="recipeDeleteContainer">
+        <section className="recipeDeleteInner">
+          <section className="crossIconContainer">
+            <FontAwesomeIcon
+              icon={faXmark}
+              className="crossIcon"
+              onClick={handleClosePrompt}
+            />
+          </section>
+          <section className="messageContainer">
+            <p>
+              Do you really want to delete this recipe? This action is
+              irreversible.
+            </p>
+          </section>
+          <section className="confirmActionContainer">
+            <button onClick={() => handleRecipeDelete(singleRecipes.id)}>
+              YES
+            </button>
+          </section>
+        </section>
+      </section>
+    </>
+  );
+};
+
+export default RecipeDeletePrompt;
