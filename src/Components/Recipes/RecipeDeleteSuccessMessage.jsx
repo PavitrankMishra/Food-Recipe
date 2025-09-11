944266beb4dd0c3134639d54d930e0967253e115
Pavitrank <pavitrank2002@gmail.com>
2025-09-11 18:41:57 +0530

    "feat: Component for Recipe Delete Success Message"

---
+++ src/Components/Recipes/RecipeDeleteSuccessMessage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./RecipeDeleteSuccessMessage.css";
+
+const RecipeDeleteSuccessMessage = () => {
+  return (
+    <>
+      <section className="recipeDeleteSuccessMessageContainer">
+        <p>Recipe Deleted Successfully</p>
+      </section>
+    </>
+  );
+};
+
+export default RecipeDeleteSuccessMessage;
