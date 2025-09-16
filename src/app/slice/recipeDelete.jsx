3bbea6e63f7ebd3b0bfa15a1150e44c2f0f2c3c2
Pavitrank <pavitrank2002@gmail.com>
2025-09-16 17:02:14 +0530

    "feat: add async thunk and slice for deleting single recipe"

---
+++ src/app/slice/recipeDelete.jsx
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const deleteSingleRecipe = createAsyncThunk(
+  "recipeDelete",
+  async (id) => {
+    const response = await fetch(
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}?key=d348a0b0-c7b8-4539-b6a6-80f883fdef51`,
+      {
+        method: "DELETE",
+      }
+    );
+
+    console.log("Response status:", response.status);
+    console.log("Response text:", response.statusText);
+
+    return { status: response.status, statusText: response.statusText };
+  }
+);
+
+const recipeDeleteSlice = createSlice({
+  name: "recipeDelete",
+  initialState: {
+    isLoading: false,
+    data: null,
+    isError: false,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(deleteSingleRecipe.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteSingleRecipe.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.data = action.payload;
+      })
+      .addCase(deleteSingleRecipe.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        console.log("Delete failed:", action.error);
+      });
+  },
+});
+
+export default recipeDeleteSlice.reducer;
