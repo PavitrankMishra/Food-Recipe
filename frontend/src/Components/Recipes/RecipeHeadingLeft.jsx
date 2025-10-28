27baf4ee8a6843017a5e34e35b832c53949f1ea7
Pavitrank <pavitrank2002@gmail.com>
2025-10-28 11:22:06 +0530

    "asset: Add microphone icon"

--- frontend/src/Components/Recipes/RecipeHeadingLeft.jsx
+++ frontend/src/Components/Recipes/RecipeHeadingLeft.jsx
@@ -3,6 +3,8 @@ import "./RecipeHeadingLeft.css";
 import Logo from "../../Assets/Logo.png";
 import { useDispatch, useSelector } from "react-redux";
 import { handleInputField } from "../../app/slice/inputValue";
+import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const RecipeHeadingLeft = () => {
   /**
@@ -21,17 +23,44 @@ const RecipeHeadingLeft = () => {
     dispatch(handleInputField(e.target.value));
   }
 
+  function handleMicClick() {
+    const getVoice = async () => {
+      try {
+        const res = await fetch("http://127.0.0.1:5000/speak");
+        if (!res.ok) {
+          throw new Error("Response was not ok");
+        }
+
+        const data = await res.json();
+        console.log("The response is: ", res);
+        console.log("The data is: ", data);
+      } catch (err) {
+        console.log("The error is: ", err);
+      }
+    };
+
+    getVoice();
+  }
+
   return (
     <>
       <img src={Logo} alt="Website Logo" />
-      <input
-        type="text"
-        placeholder="Search over 1,00,000 recipes"
-        className="inputField"
-        value={inputValue}
-        onChange={updateInputValue}
-        id="inputFieldId"
-      />
+      <section className="inputMicContainer">
+        <input
+          type="text"
+          placeholder="Search over 1,00,000 recipes"
+          className="inputField"
+          value={inputValue}
+          onChange={updateInputValue}
+          id="inputFieldId"
+        />
+        <FontAwesomeIcon
+          icon={faMicrophone}
+          className="microphone"
+          size="xl"
+          onClick={handleMicClick}
+        />
+      </section>
     </>
   );
 };
--- frontend/src/Components/Recipes/Recipes.jsx
+++ frontend/src/Components/Recipes/Recipes.jsx
@@ -201,6 +201,7 @@ const Recipes = () => {
       )}
       {recipeDeletedSuccessfull && <RecipeDeleteSuccessMessage />}
       {recipeDeletedFail && <RecipeDeleteFailMessage />}
+      
     </>
   );
 };
